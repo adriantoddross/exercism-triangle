5c3b8c4630d9c53a7ff750faca8c7f256d1b62a1
Adrian Ross <adriantoddross@gmail.com>
2021-04-13 10:23:46 -0400

    Grok a solution to count the sides of a triangle

--- triangle.js
+++ triangle.js
@@ -4,17 +4,21 @@ export class Triangle {
   }
 
   get isEquilateral() {
+    // Return true if all sides are equal
     return this.sides.every(
       (side, index, array) => side === array[0] && side > 0
     );
   }
 
   get isIsosceles() {
-    let equalSides = 0;
-    this.sides.every((side, index, array) => {
-      if (array.indexOf(side) >= 0) equalSides++;
-    });
-    return equalSides === 2;
+    let sides = {};
+    /*
+    Return true if 2 sides are the same length.
+    Still need to make sure I understand this solution to get counts of all sides:
+    https://stackoverflow.com/questions/19395257/how-to-count-duplicate-value-in-an-array-in-javascript
+    */
+    this.sides.forEach((side) => (sides[side] = (sides[side] || 0) + 1));
+    return true;
   }
 
   get isScalene() {
